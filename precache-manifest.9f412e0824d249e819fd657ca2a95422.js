a491a5e2f1df13914cda9ec9e69d80329f507780
alittlebyte <vladislavnikora@gmail.com>
2020-07-20 20:03:54 +0300

    Updates

---
+++ precache-manifest.9f412e0824d249e819fd657ca2a95422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de4c5e3952f54e1ce507bf4459d01c3",
+    "url": "/roskusstrah/index.html"
+  },
+  {
+    "revision": "1a1c180477245ab76d1a",
+    "url": "/roskusstrah/static/js/2.3ddb09c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roskusstrah/static/js/2.3ddb09c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00245bc0d201ba68b87f",
+    "url": "/roskusstrah/static/js/main.6c08ce65.chunk.js"
+  },
+  {
+    "revision": "d952d746c53354bc15e5",
+    "url": "/roskusstrah/static/js/runtime-main.5398e5a7.js"
+  }
+]);
